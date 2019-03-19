178de105e994d34e3af18aaa747b4e50750e0cd1
MB <sr.funk.sensei@gmail.com>
2019-03-19 15:21:08 +0100

    initial commit

---
+++ src/main/java/com/mb/VideoRentalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalStoreApplication.class, args);
+	}
+
+}
