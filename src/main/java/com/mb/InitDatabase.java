1fc9595f78f12c0fe156b8c47ab5680a1117f3c9
MB <sr.funk.sensei@gmail.com>
2019-03-27 09:28:21 +0100

    added setting up price to db init method

--- src/main/java/com/mb/InitDatabase.java
+++ src/main/java/com/mb/InitDatabase.java
@@ -1,8 +1,11 @@
 package com.mb;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.AbstractMap;
 import java.util.Collections;
+import java.util.Currency;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,19 +19,24 @@ import com.mb.model.FilmType;
 import com.mb.model.bonusPoint.BonusPoint;
 import com.mb.model.bonusPoint.NewReleaseBonusPoint;
 import com.mb.model.bonusPoint.RegularBonusPoint;
+import com.mb.model.price.BasicPrice;
+import com.mb.model.price.PremiumPrice;
 import com.mb.repository.FilmRepository;
+import com.mb.repository.price.BasicPriceRepository;
+import com.mb.repository.price.PremiumPriceRepository;
+
+import lombok.AllArgsConstructor;
 
 @Component
+@AllArgsConstructor
 public class InitDatabase {
 
 	private final FilmRepository filmRepository;
-
-	InitDatabase(FilmRepository filmRepository) {
-		this.filmRepository = filmRepository;
-	}
+	private final BasicPriceRepository basicPriceRepository;
+	private final PremiumPriceRepository premiumPriceRepository;
 
 	@Bean
-	CommandLineRunner loadFilms() {	
+	CommandLineRunner init() {	
 		final LocalDate now = LocalDate.now();
 		
 		final Map<FilmType, BonusPoint> bonusPoints = Stream.of( 
@@ -40,6 +48,11 @@ public class InitDatabase {
 		    Collections::<FilmType, BonusPoint> unmodifiableMap));
 		
 		return args -> {
+			final Currency sek = Currency.getInstance(new Locale("sv", "SE"));
+			
+			basicPriceRepository.save(new BasicPrice(now, now, sek.getSymbol(), new BigDecimal(30)));
+			premiumPriceRepository.save(new PremiumPrice(now, now, sek.getSymbol(), new BigDecimal(40)));
+			
 			filmRepository.save(new Film(now, now, "Lord of the rings: The Fellowship of the Ring", 2001, FilmType.OLD, bonusPoints.get(FilmType.OLD)));
 			filmRepository.save(new Film(now, now, "Lord of the rings: The Two Towers", 2002, FilmType.REGULAR, bonusPoints.get(FilmType.REGULAR)));
 			filmRepository.save(new Film(now, now, "Lord of the rings: The Return of the King", 2003, FilmType.NEW, bonusPoints.get(FilmType.NEW)));
