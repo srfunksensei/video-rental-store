3c8d8d2b275a09e38c8cff93b14421b3cbcf7fa8
MB <sr.funk.sensei@gmail.com>
2019-03-28 11:58:34 +0100

    cleanup of FilmInventoryController

--- src/main/java/com/mb/controller/FilmInventoryController.java
+++ src/main/java/com/mb/controller/FilmInventoryController.java
@@ -3,11 +3,8 @@ package com.mb.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
-import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,55 +15,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mb.assembler.FilmResourceAssembler;
-import com.mb.model.Film;
+import com.mb.assembler.resource.FilmResource;
 import com.mb.model.FilmType;
-import com.mb.repository.FilmRepository;
-import com.mb.repository.specification.FilmSpecificationsBuilder;
+import com.mb.service.FilmService;
 
 @RestController
 @RequestMapping(value = "/films", produces = "application/hal+json")
 public class FilmInventoryController {
 
-	private final FilmRepository filmRepository;
-
-	private final FilmResourceAssembler filmResourceAssembler;
+	private final FilmService filmService;
 
 	@Autowired
-	public FilmInventoryController(final FilmRepository filmRepository,
-			final FilmResourceAssembler filmResourceAssembler) {
-		this.filmRepository = filmRepository;
-		this.filmResourceAssembler = filmResourceAssembler;
+	public FilmInventoryController(final FilmService filmService) {
+		this.filmService = filmService;
 	}
 
 	@GetMapping(value = "/{filmId}")
-	public ResponseEntity<Resource<Film>> findOne(@PathVariable Long filmId) {
-		return filmRepository.findById(filmId) //
-				.map(filmResourceAssembler::toResource) //
+	public ResponseEntity<FilmResource> findOne(@PathVariable Long filmId) {
+		return filmService.findOne(filmId) //
 				.map(ResponseEntity::ok) //
 				.orElse(ResponseEntity.notFound().build());
 	}
 
 	@DeleteMapping(value = "/{filmId}")
 	public void deleteOne(@PathVariable Long filmId) {
-		filmRepository.deleteById(filmId);
+		filmService.deleteOne(filmId);
 	}
 
 	@GetMapping
-	public HttpEntity<PagedResources<Resource<Film>>> findAll( //
+	public HttpEntity<PagedResources<FilmResource>> findAll( //
 			@RequestParam("title") Optional<String> title, //
 			@RequestParam("type") Optional<FilmType> type, //
-			Pageable pageable, PagedResourcesAssembler<Film> pagedAssembler) {
-		
-		final FilmSpecificationsBuilder builder = new FilmSpecificationsBuilder();
-		if (title.isPresent()) {
-			builder.with(FilmSpecificationsBuilder.TITLE_SEARCH_KEY, title.get());
-		}
-		if (type.isPresent()) {
-			builder.with(FilmSpecificationsBuilder.TYPE_SEARCH_KEY, type.get().name());
-		}
-
-		final Page<Film> films = filmRepository.findAll(builder.build(), pageable);
-		return new ResponseEntity<>(pagedAssembler.toResource(films, filmResourceAssembler), HttpStatus.OK);
+			Pageable pageable) {
+		return new ResponseEntity<>(filmService.findAll(title, type, pageable), HttpStatus.OK);
 	}
 }
