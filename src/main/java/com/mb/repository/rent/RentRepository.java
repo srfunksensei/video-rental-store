a6831776071db9fb7eff13ea449efc884fab33a7
MB <sr.funk.sensei@gmail.com>
2019-04-11 08:06:15 +0200

    added support for fetching pagable rentals

--- src/main/java/com/mb/repository/rent/RentRepository.java
+++ src/main/java/com/mb/repository/rent/RentRepository.java
@@ -1,10 +1,14 @@
 package com.mb.repository.rent;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mb.model.rental.Rental;
 
 @Repository("rentRepository")
-public interface RentRepository extends CrudRepository<Rental, Long> {
+public interface RentRepository extends JpaRepository<Rental, Long> {
+	
+	Page<Rental> findAll(Pageable pageable);
 }
