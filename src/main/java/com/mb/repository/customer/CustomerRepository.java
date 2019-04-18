9994b175d13eb10fbdc119d31a1c447402cd32a9
MB <sr.funk.sensei@gmail.com>
2019-04-18 10:32:26 +0200

    added CustomerRepository

---
+++ src/main/java/com/mb/repository/customer/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.mb.repository.customer;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mb.model.customer.Customer;
+
+@Repository("customerRepository")
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	Page<Customer> findAll(Pageable pageable);
+}
