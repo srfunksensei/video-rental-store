e927b0a1fbfd0ea800f9c5ff626262e26ad778b6
srfunksensei <sr.funk.sensei@gmail.com>
2021-06-22 11:50:53 +0200

    cleaned up OldFilmPriceCalculator and added tests

--- src/main/java/com/mb/service/rent/calculation/OldFilmPriceCalculator.java
+++ src/main/java/com/mb/service/rent/calculation/OldFilmPriceCalculator.java
@@ -2,6 +2,7 @@ package com.mb.service.rent.calculation;
 
 import java.math.BigDecimal;
 
+import com.mb.exception.InvalidDataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,19 +17,22 @@ public class OldFilmPriceCalculator extends FilmPriceCalculator<BasicPrice> {
 	public static final long NUM_OF_FIRST_DAYS_WITH_SAME_PRICE = 5;
 	
 	@Autowired
-	public OldFilmPriceCalculator(BasicPriceRepository priceRepository) {
+	public OldFilmPriceCalculator(final BasicPriceRepository priceRepository) {
 		super(priceRepository);
 	}
 
 	@Override
-	public PriceDto calculatePrice(long numOfDays) {
+	public PriceDto calculatePrice(final long numOfDays) {
+		if (numOfDays < 0) {
+			throw new InvalidDataException();
+		}
+
 		final PriceDto price = getBasePrice();
-		
 		final BigDecimal value = calculatePrice(price.getValue(), numOfDays);
 		return new PriceDto(value, price.getCurrency());
 	}
 	
-	private BigDecimal calculatePrice(final BigDecimal baseValue, long numOfDays) {
+	private BigDecimal calculatePrice(final BigDecimal baseValue, final long numOfDays) {
 		if (numOfDays > OldFilmPriceCalculator.NUM_OF_FIRST_DAYS_WITH_SAME_PRICE) {
 			final long dateDiff = numOfDays - OldFilmPriceCalculator.NUM_OF_FIRST_DAYS_WITH_SAME_PRICE;
 			return baseValue.add(baseValue.multiply(new BigDecimal(dateDiff)));
---
+++ src/test/java/com/mb/service/rent/calculation/OldFilmPriceCalculatorTest.java
@@ -0,0 +1,66 @@
+package com.mb.service.rent.calculation;
+
+import com.mb.dto.PriceDto;
+import com.mb.exception.InvalidDataException;
+import com.mb.model.price.BasicPrice;
+import com.mb.repository.price.BasicPriceRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OldFilmPriceCalculatorTest {
+
+    @MockBean
+    public BasicPriceRepository basicPriceRepository;
+
+    @Autowired
+    public OldFilmPriceCalculator oldFilmPriceCalculator;
+
+    @Test(expected = InvalidDataException.class)
+    public void calculatePrice_negativeDays() {
+        oldFilmPriceCalculator.calculatePrice(-1L);
+    }
+
+    @Test
+    public void calculatePrice_samePriceForNDays() {
+        final BasicPrice price = new BasicPrice();
+        price.setValue(BigDecimal.ONE);
+        price.setCurrencySymbol("RSD");
+
+        final List<BasicPrice> list = Collections.singletonList(price);
+        when(basicPriceRepository.findAll()).thenReturn(list);
+
+        final long numOfDays = OldFilmPriceCalculator.NUM_OF_FIRST_DAYS_WITH_SAME_PRICE - 2;
+        final PriceDto priceDto = oldFilmPriceCalculator.calculatePrice(numOfDays);
+
+        Assert.assertEquals(price.getValue(), priceDto.getValue());
+    }
+
+    @Test
+    public void calculatePrice_differentPriceAfterNDays() {
+        final BasicPrice price = new BasicPrice();
+        price.setValue(BigDecimal.ONE);
+        price.setCurrencySymbol("RSD");
+
+        final List<BasicPrice> list = Collections.singletonList(price);
+        when(basicPriceRepository.findAll()).thenReturn(list);
+
+        final long numOfDays = OldFilmPriceCalculator.NUM_OF_FIRST_DAYS_WITH_SAME_PRICE + 2;
+        final PriceDto priceDto = oldFilmPriceCalculator.calculatePrice(numOfDays);
+
+        final long diff = numOfDays - OldFilmPriceCalculator.NUM_OF_FIRST_DAYS_WITH_SAME_PRICE;
+        Assert.assertEquals(price.getValue().add(price.getValue().multiply(new BigDecimal(diff))), priceDto.getValue());
+    }
+}
